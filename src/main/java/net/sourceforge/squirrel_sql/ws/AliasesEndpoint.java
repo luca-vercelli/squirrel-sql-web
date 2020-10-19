94845770f68c9ae092d840092a6a0c7160e0a8ed
luca-vercelli <luca.vercelli.to@gmail.com>
2020-10-19 23:26:47 +0200

    aliases

--- src/main/java/net/sourceforge/squirrel_sql/ws/AliasesEndpoint.java
+++ src/main/java/net/sourceforge/squirrel_sql/ws/AliasesEndpoint.java
@@ -1,6 +1,7 @@
 package net.sourceforge.squirrel_sql.ws;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.ws.rs.DELETE;
@@ -9,6 +10,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 
+import net.sourceforge.squirrel_sql.client.gui.db.AliasesAndDriversManager;
+import net.sourceforge.squirrel_sql.client.gui.db.SQLAlias;
 import net.sourceforge.squirrel_sql.fw.sql.ISQLAlias;
 
 @Path("/")
@@ -17,30 +20,43 @@ public class AliasesEndpoint {
 	@Inject
 	WebApp webapp;
 
+	// We cannot inject the manager, as it is not an EJB
+	// Instead, we inject the WebApp and retrieve the manager
+	private AliasesAndDriversManager getManager() {
+		return webapp.getSwingApp().getAliasesAndDriversManager();
+	}
+
 	@GET
 	@Path("/Aliases")
 	public List<? extends ISQLAlias> getItems() {
-		return webapp.getSwingApp().getAliasesAndDriversManager().getAliasList();
+		return getManager().getAliasList();
 	}
 
 	@GET
 	@Path("/Aliases/{name}")
-	public List<String> getItem(String name) {
-		return null;
+	public ISQLAlias getItem(String name) {
+		List<ISQLAlias> items = getManager().getAliasList().stream().filter(x -> x.getName().equals(name))
+				.collect(Collectors.toList());
+		return items.isEmpty() ? null : items.get(0);
 	}
 
 	@POST
 	@Path("/Aliases")
 	public void createItem(ISQLAlias item) {
+		getManager().addAlias(item);
+		// should call saveAliases() ?
 	}
 
 	@PUT
 	@Path("/Aliases/{name}")
 	public void updateItem(ISQLAlias item) {
+		// TODO
 	}
 
 	@DELETE
 	@Path("/Aliases/{name}")
-	public void deleteItem(String name) {
+	public void deleteItem(SQLAlias item) {
+		getManager().removeAlias(item);
+		// should call saveAliases() ?
 	}
 }
