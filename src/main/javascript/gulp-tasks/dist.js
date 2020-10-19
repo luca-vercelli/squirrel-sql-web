42472c0ba221ade1386e9dab48a419dbe829321d
Luca Vercelli <luca.vercelli.to@gmail.com>
2020-10-19 12:46:37 +0200

    build

--- src/main/javascript/gulp-tasks/dist.js
+++ src/main/javascript/gulp-tasks/dist.js
@@ -1,9 +1,15 @@
 'use strict'
 var gulp = require('gulp');
 var destfolder = '../webapp';
+const del = require('del');
 
 gulp.task('dist', function () {
-    return gulp.src(['./index.html', 'assets', 'main'])
+    return gulp.src(['index.html', 'assets/**/*', 'main/**/*'], { "base" : "." })
         .pipe(gulp.dest(destfolder));
 });
 
+gulp.task('clean:dist', function () {
+    return del([
+      destfolder + '/assets', destfolder + '/main', destfolder + '/*.html*'
+    ]);
+});
