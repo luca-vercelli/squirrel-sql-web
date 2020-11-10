58b4ee97785445e8b64415b0c67f8a0d78f091dc
Luca Vercelli <luca.vercelli.to@gmail.com>
2020-11-10 15:47:00 +0100

    clean

--- src/main/java/net/sourceforge/squirrel_sql/ws/managers/SessionsManager.java
+++ src/main/java/net/sourceforge/squirrel_sql/ws/managers/SessionsManager.java
@@ -1,10 +1,6 @@
 package net.sourceforge.squirrel_sql.ws.managers;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -16,17 +12,11 @@ import javax.inject.Inject;
 import org.apache.log4j.Logger;
 
 import net.sourceforge.squirrel_sql.client.gui.db.SQLAlias;
-import net.sourceforge.squirrel_sql.client.session.ISQLExecuterHandler;
 import net.sourceforge.squirrel_sql.client.session.ISession;
-import net.sourceforge.squirrel_sql.client.session.SQLExecuterTask;
-import net.sourceforge.squirrel_sql.client.session.mainpanel.SQLExecutionHandlerPublic;
 import net.sourceforge.squirrel_sql.fw.id.IIdentifier;
 import net.sourceforge.squirrel_sql.fw.id.IntegerIdentifier;
-import net.sourceforge.squirrel_sql.fw.id.UidIdentifier;
 import net.sourceforge.squirrel_sql.fw.sql.SQLConnection;
 import net.sourceforge.squirrel_sql.fw.sql.SQLDriver;
-import net.sourceforge.squirrel_sql.fw.util.StringUtilities;
-import net.sourceforge.squirrel_sql.ws.model.Table;
 import net.sourceforge.squirrel_sql.ws.resources.SessionsEndpoint;
 
 /**
@@ -149,52 +139,4 @@ public class SessionsManager {
 		return id;
 	}
 
-	// In SQuirreLSQL Core, the SQLExecuterTask is reached through the chain:
-	// Session -> SessionPanel -> MainPanel -> SQLPanel.runCurrentExecuter() ->
-	// SQLResultExecuterPanel -> SQLExecutionHandler -> SQLExecuterTask
-	// and the Runnable is executed within a ThreadPool
-	// We don't need such a mess
-	public Table executeQuery(String sessionId, String query) throws SQLException {
-		ISession session = getSessionById(sessionId);
-
-		query = StringUtilities.cleanString(query);
-		logger.info("Running query: " + query);
-
-		Connection conn = session.getSQLConnection().getConnection();
-		try (Statement stmt = conn.createStatement()) {
-			try (ResultSet rs = stmt.executeQuery(query)) {
-				return rs2table(rs);
-			}
-		}
-	}
-
-	/**
-	 * Convert a ResultSet into a Table
-	 * 
-	 * @param rs
-	 * @return
-	 * @throws SQLException
-	 */
-	public Table rs2table(ResultSet rs) throws SQLException {
-		Table t = new Table();
-
-		// Metadata
-		ResultSetMetaData md = rs.getMetaData();
-		String[] names = new String[md.getColumnCount()];
-		for (int i = 0; i < names.length; ++i) {
-			names[i] = md.getColumnName(i + 1);
-		}
-		t.setColumnHeaders(names);
-
-		// Data
-		while (rs.next()) {
-			Object[] row = new Object[names.length];
-			for (int i = 0; i < names.length; ++i) {
-				row[i] = rs.getObject(i + 1);
-			}
-			t.getRows().add(row);
-		}
-
-		return t;
-	}
 }
