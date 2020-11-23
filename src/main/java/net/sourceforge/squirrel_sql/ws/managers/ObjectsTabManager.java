e4de2cf64adcd02ac03b588b3bc6478c64fa10ba
Luca Vercelli <luca.vercelli.to@gmail.com>
2020-11-23 19:45:12 +0100

    manager

--- src/main/java/net/sourceforge/squirrel_sql/ws/managers/ObjectsTabManager.java
+++ src/main/java/net/sourceforge/squirrel_sql/ws/managers/ObjectsTabManager.java
@@ -1,10 +1,19 @@
 package net.sourceforge.squirrel_sql.ws.managers;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import org.apache.log4j.Logger;
 
+import net.sourceforge.squirrel_sql.client.session.ISession;
+import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.INodeExpander;
+import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.ObjectTreeNode;
+import net.sourceforge.squirrel_sql.client.session.mainpanel.objecttree.expanders.DatabaseExpander;
+import net.sourceforge.squirrel_sql.fw.sql.DatabaseObjectInfo;
+import net.sourceforge.squirrel_sql.fw.sql.DatabaseObjectType;
 import net.sourceforge.squirrel_sql.ws.resources.SessionsEndpoint;
 
 /**
@@ -22,5 +31,31 @@ public class ObjectsTabManager {
 	SessionsManager sessionsManager;
 
 	Logger logger = Logger.getLogger(SessionsEndpoint.class);
+
+	public ObjectTreeNode createRootNode(ISession session) {
+		DatabaseObjectInfo info = new DatabaseObjectInfo(null, null, session.getAlias().getName(),
+				DatabaseObjectType.SESSION, session.getMetaData());
+		// this is essentially a RootNode
+		ObjectTreeNode node = new ObjectTreeNode(session, info);
+		return node;
+	}
+
+	public List<ObjectTreeNode> expandSessionNode(ISession session) throws SQLException {
+		ObjectTreeNode node = createRootNode(session);
+		return expandSessionNode(node);
+	}
+
+	protected List<ObjectTreeNode> expandSessionNode(ObjectTreeNode node) throws SQLException {
+		INodeExpander expander = new DatabaseExpander(node.getSession());
+		return expander.createChildren(node.getSession(), node);
+	}
+
+	protected List<ObjectTreeNode> expandNode(ObjectTreeNode node) throws SQLException {
+		if (node.getDatabaseObjectType() == DatabaseObjectType.SESSION) {
+			return expandSessionNode(node);
+		}
+		// TODO
+		return null;
+	}
 
 }
