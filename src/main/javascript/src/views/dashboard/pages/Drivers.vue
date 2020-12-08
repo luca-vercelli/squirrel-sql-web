7eaf848fdfecef669a6605b5f798a44eeae248e6
luca-vercelli <luca.vercelli.to@gmail.com>
2020-12-08 20:05:12 +0100

    drivers

--- src/main/javascript/src/views/dashboard/pages/Drivers.vue
+++ src/main/javascript/src/views/dashboard/pages/Drivers.vue
@@ -98,91 +98,61 @@
         </tbody>
       </v-simple-table>
     </base-material-card>
-
-    <div class="py-3" />
-
-    <base-material-card
-      color="success"
-      dark
-      icon="mdi-clipboard-plus"
-      title="Table on Dark Background"
-      class="px-5 py-3"
-    >
-      <v-simple-table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Country</th>
-            <th>City</th>
-            <th class="text-right">
-              Salary
-            </th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Dakota Rice</td>
-            <td>Niger</td>
-            <td>Oud-Turnhout</td>
-            <td class="text-right">
-              $36,738
-            </td>
-          </tr>
-
-          <tr>
-            <td>2</td>
-            <td>Minverva Hooper</td>
-            <td>Curaçao</td>
-            <td>Sinaas-Waas</td>
-            <td class="text-right">
-              $23,789
-            </td>
-          </tr>
-
-          <tr>
-            <td>3</td>
-            <td>Sage Rodriguez</td>
-            <td>Netherlands</td>
-            <td>Baileux</td>
-            <td class="text-right">
-              $56,142
-            </td>
-          </tr>
-
-          <tr>
-            <td>4</td>
-            <td>Philip Chaney</td>
-            <td>Korea, South</td>
-            <td>Overland Park</td>
-            <td class="text-right">
-              $38,735
-            </td>
-          </tr>
-
-          <tr>
-            <td>5</td>
-            <td>Doris Greene</td>
-            <td>Malawi</td>
-            <td>Feldkirchen in Kärnten</td>
-            <td class="text-right">
-              $63,542
-            </td>
-          </tr>
-
-          <tr>
-            <td>6</td>
-            <td>Mason Porter</td>
-            <td>Chile</td>
-            <td>Gloucester</td>
-            <td class="text-right">
-              $78,615
-            </td>
-          </tr>
-        </tbody>
-      </v-simple-table>
-    </base-material-card>
   </v-container>
 </template>
+
+<script>
+  export default {
+    name: 'Drivers',
+
+    data () {
+      return {
+        enableMock: true,
+      }
+    },
+
+    computed: {
+      wsUrl: this.enableMock ? '../mock/Drivers.json' : '../ws/Drivers',
+    },
+
+    methods: {
+      loadDrivers: function () {
+        /*
+        var url = this.wsUrl
+        $.ajax({
+          url: url,
+          dataType: 'json',
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('authToken'),
+          },
+          success: function (response) {
+            this.drivers = response.data
+            this.drivers.sort(cmpNames);
+            var menu = $('#ui-sub-menu-drivers').find('nav');
+            menu.html("");
+            createMenuEntry(menu, 'driver.html', 'Create new');
+            var checkIcon = ' <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">check</i>';
+            for(var i in drivers) {
+              var driver = drivers[i];
+              if (driver.identifier != null) {
+                var isChecked = driver.jdbcdriverClassLoaded ? checkIcon : ''
+                createMenuEntry(menu, 'driver.html?id=' + driver.identifier, driver.name + isChecked);
+              } else {
+                console.log(`Skipping entry ${driver.name} with no identifier`); // should not happen
+              }
+            };
+
+            for (i in drivers_callbacks) {
+              drivers_callbacks[i]();
+            }
+          }
+        }); */
+      },
+      cmpNames: function (x, y) {
+        var name1 = x.name ? x.name.toLowerCase() : ''
+        var name2 = y.name ? y.name.toLowerCase() : ''
+        return name1 < name2 ? -1 : name1 === name2 ? 0 : +1
+      },
+    },
+  }
+</script>
