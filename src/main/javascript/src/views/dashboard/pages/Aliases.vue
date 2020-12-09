0d20a5d6baaf5725408186fc01ddbc6ea4f7c5d9
luca-vercelli <luca.vercelli.to@gmail.com>
2020-12-09 23:47:34 +0100

    aliases

--- src/main/javascript/src/views/dashboard/pages/Aliases.vue
+++ src/main/javascript/src/views/dashboard/pages/Aliases.vue
@@ -4,181 +4,70 @@
     fluid
     tag="section"
   >
-    <base-v-component
-      heading="Simple Tables"
-      link="components/simple-tables"
-    />
-
     <base-material-card
       icon="mdi-clipboard-text"
-      title="Simple Table"
+      title="Aliases"
       class="px-5 py-3"
     >
       <v-simple-table>
         <thead>
           <tr>
-            <th class="primary--text">
-              ID
-            </th>
             <th class="primary--text">
               Name
             </th>
-            <th class="primary--text">
-              Country
-            </th>
-            <th class="primary--text">
-              City
-            </th>
             <th class="text-right primary--text">
-              Salary
+              Actions
             </th>
           </tr>
         </thead>
 
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Dakota Rice</td>
-            <td>Niger</td>
-            <td>Oud-Turnhout</td>
+          <tr
+            v-for="alias in aliases"
+            :key="alias.identifier.string"
+          >
+            <td>{{ alias.name }}</td>
             <td class="text-right">
-              $36,738
-            </td>
-          </tr>
-
-          <tr>
-            <td>2</td>
-            <td>Minverva Hooper</td>
-            <td>Curaçao</td>
-            <td>Sinaas-Waas</td>
-            <td class="text-right">
-              $23,789
-            </td>
-          </tr>
-
-          <tr>
-            <td>3</td>
-            <td>Sage Rodriguez</td>
-            <td>Netherlands</td>
-            <td>Baileux</td>
-            <td class="text-right">
-              $56,142
-            </td>
-          </tr>
-
-          <tr>
-            <td>4</td>
-            <td>Philip Chaney</td>
-            <td>Korea, South</td>
-            <td>Overland Park</td>
-            <td class="text-right">
-              $38,735
-            </td>
-          </tr>
-
-          <tr>
-            <td>5</td>
-            <td>Doris Greene</td>
-            <td>Malawi</td>
-            <td>Feldkirchen in Kärnten</td>
-            <td class="text-right">
-              $63,542
-            </td>
-          </tr>
-
-          <tr>
-            <td>6</td>
-            <td>Mason Porter</td>
-            <td>Chile</td>
-            <td>Gloucester</td>
-            <td class="text-right">
-              $78,615
-            </td>
-          </tr>
-        </tbody>
-      </v-simple-table>
-    </base-material-card>
-
-    <div class="py-3" />
-
-    <base-material-card
-      color="success"
-      dark
-      icon="mdi-clipboard-plus"
-      title="Table on Dark Background"
-      class="px-5 py-3"
-    >
-      <v-simple-table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Country</th>
-            <th>City</th>
-            <th class="text-right">
-              Salary
-            </th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Dakota Rice</td>
-            <td>Niger</td>
-            <td>Oud-Turnhout</td>
-            <td class="text-right">
-              $36,738
-            </td>
-          </tr>
-
-          <tr>
-            <td>2</td>
-            <td>Minverva Hooper</td>
-            <td>Curaçao</td>
-            <td>Sinaas-Waas</td>
-            <td class="text-right">
-              $23,789
-            </td>
-          </tr>
-
-          <tr>
-            <td>3</td>
-            <td>Sage Rodriguez</td>
-            <td>Netherlands</td>
-            <td>Baileux</td>
-            <td class="text-right">
-              $56,142
-            </td>
-          </tr>
-
-          <tr>
-            <td>4</td>
-            <td>Philip Chaney</td>
-            <td>Korea, South</td>
-            <td>Overland Park</td>
-            <td class="text-right">
-              $38,735
-            </td>
-          </tr>
-
-          <tr>
-            <td>5</td>
-            <td>Doris Greene</td>
-            <td>Malawi</td>
-            <td>Feldkirchen in Kärnten</td>
-            <td class="text-right">
-              $63,542
-            </td>
-          </tr>
-
-          <tr>
-            <td>6</td>
-            <td>Mason Porter</td>
-            <td>Chile</td>
-            <td>Gloucester</td>
-            <td class="text-right">
-              $78,615
+              <button
+                class="v-btn v-size--default success"
+                title="Edit"
+                @click="connect"
+              >
+                <i
+                  aria-hidden="true"
+                  class="v-icon notranslate mdi mdi-connection theme--dark"
+                />
+              </button> &nbsp;
+              <button
+                class="v-btn v-size--default success"
+                title="Edit"
+                @click="editAlias"
+              >
+                <i
+                  aria-hidden="true"
+                  class="v-icon notranslate mdi mdi-pencil theme--dark"
+                />
+              </button> &nbsp;
+              <button
+                class="v-btn v-size--default success"
+                title="Clone"
+                @click="cloneAlias"
+              >
+                <i
+                  aria-hidden="true"
+                  class="v-icon notranslate mdi mdi-content-copy theme--dark"
+                />
+              </button> &nbsp;
+              <button
+                class="v-btn v-size--default error"
+                title="Delete"
+                @click="deleteAlias"
+              >
+                <i
+                  aria-hidden="true"
+                  class="v-icon notranslate mdi mdi-delete theme--dark"
+                />
+              </button>
             </td>
           </tr>
         </tbody>
@@ -186,3 +75,58 @@
     </base-material-card>
   </v-container>
 </template>
+
+<script>
+  var $ = require('jquery')
+  export default {
+    name: 'Aliases',
+
+    data () {
+      return {
+        aliases: [],
+        enableMock: true,
+      }
+    },
+
+    computed: {
+      wsUrl: function () {
+        return this.enableMock ? process.env.BASE_URL + 'mock/Aliases.json' : '../ws/Aliases'
+      },
+    },
+
+    created: function () {
+      this.loadDrivers()
+    },
+
+    methods: {
+      loadDrivers: function () {
+        var url = this.wsUrl
+        var that = this
+        $.ajax({
+          url: url,
+          dataType: 'json',
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('authToken'),
+          },
+          success: function (response) {
+            that.aliases = response.data
+            that.aliases.sort(that.cmpNames)
+          },
+        })
+      },
+      cmpNames: function (x, y) {
+        var name1 = x.name ? x.name.toLowerCase() : ''
+        var name2 = y.name ? y.name.toLowerCase() : ''
+        return name1 < name2 ? -1 : name1 > name2 ? +1 : 0
+      },
+      connect: function () {
+      },
+      editAlias: function () {
+      },
+      cloneAlias: function () {
+      },
+      deleteAlias: function () {
+      },
+    },
+  }
+</script>
