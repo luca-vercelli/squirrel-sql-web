e4b8573579fec911af8c080d35976fa11804bf96
luca-vercelli <luca.vercelli.to@gmail.com>
2020-12-09 23:29:44 +0100

    drivers

--- src/main/javascript/src/views/dashboard/pages/Drivers.vue
+++ src/main/javascript/src/views/dashboard/pages/Drivers.vue
@@ -18,81 +18,33 @@
         <thead>
           <tr>
             <th class="primary--text">
-              ID
-            </th>
-            <th class="primary--text">
               Name
             </th>
             <th class="primary--text">
-              Country
-            </th>
-            <th class="primary--text">
-              City
-            </th>
-            <th class="text-right primary--text">
-              Salary
+              Actions
             </th>
           </tr>
         </thead>
 
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Dakota Rice</td>
-            <td>Niger</td>
-            <td>Oud-Turnhout</td>
-            <td class="text-right">
-              $36,738
-            </td>
-          </tr>
-
-          <tr>
-            <td>2</td>
-            <td>Minverva Hooper</td>
-            <td>Curaçao</td>
-            <td>Sinaas-Waas</td>
-            <td class="text-right">
-              $23,789
-            </td>
-          </tr>
-
-          <tr>
-            <td>3</td>
-            <td>Sage Rodriguez</td>
-            <td>Netherlands</td>
-            <td>Baileux</td>
-            <td class="text-right">
-              $56,142
-            </td>
-          </tr>
-
-          <tr>
-            <td>4</td>
-            <td>Philip Chaney</td>
-            <td>Korea, South</td>
-            <td>Overland Park</td>
-            <td class="text-right">
-              $38,735
-            </td>
-          </tr>
-
-          <tr>
-            <td>5</td>
-            <td>Doris Greene</td>
-            <td>Malawi</td>
-            <td>Feldkirchen in Kärnten</td>
-            <td class="text-right">
-              $63,542
-            </td>
-          </tr>
-
-          <tr>
-            <td>6</td>
-            <td>Mason Porter</td>
-            <td>Chile</td>
-            <td>Gloucester</td>
-            <td class="text-right">
-              $78,615
+          <tr
+            v-for="driver in drivers"
+            :key="driver.identifier"
+          >
+            <td>{{ driver.name }}</td>
+            <td>
+              <button class="v-btn v-size--default secondary">
+                Connect
+              </button> &nbsp;
+              <button class="v-btn v-size--default secondary">
+                Edit
+              </button> &nbsp;
+              <button class="v-btn v-size--default secondary">
+                Clone
+              </button> &nbsp;
+              <button class="v-btn v-size--default error">
+                Delete
+              </button>
             </td>
           </tr>
         </tbody>
@@ -102,23 +54,41 @@
 </template>
 
 <script>
+  var $ = require('jquery')
   export default {
     name: 'Drivers',
 
     data () {
       return {
+        drivers: [],
         enableMock: true,
       }
     },
 
     computed: {
-      wsUrl: this.enableMock ? '../mock/Drivers.json' : '../ws/Drivers',
+      wsUrl: function () {
+        return this.enableMock ? process.env.BASE_URL + 'mock/Drivers.json' : '../ws/Drivers'
+      },
+      /* driversComputed: function () {
+        return this.drivers
+      }, */
+    },
+
+    watch: {
+      drivers: function (val) {
+        console.log('here')
+        this.driversComputed = val
+      },
+    },
+
+    created: function () {
+      this.loadDrivers()
     },
 
     methods: {
       loadDrivers: function () {
-        /*
         var url = this.wsUrl
+        var that = this
         $.ajax({
           url: url,
           dataType: 'json',
@@ -126,32 +96,16 @@
             Authorization: 'Bearer ' + localStorage.getItem('authToken'),
           },
           success: function (response) {
-            this.drivers = response.data
-            this.drivers.sort(cmpNames);
-            var menu = $('#ui-sub-menu-drivers').find('nav');
-            menu.html("");
-            createMenuEntry(menu, 'driver.html', 'Create new');
-            var checkIcon = ' <i class="material-icons mdc-list-item__start-detail mdc-drawer-item-icon" aria-hidden="true">check</i>';
-            for(var i in drivers) {
-              var driver = drivers[i];
-              if (driver.identifier != null) {
-                var isChecked = driver.jdbcdriverClassLoaded ? checkIcon : ''
-                createMenuEntry(menu, 'driver.html?id=' + driver.identifier, driver.name + isChecked);
-              } else {
-                console.log(`Skipping entry ${driver.name} with no identifier`); // should not happen
-              }
-            };
-
-            for (i in drivers_callbacks) {
-              drivers_callbacks[i]();
-            }
-          }
-        }); */
+            that.drivers = response.data
+            that.drivers.sort(that.cmpNames)
+            console.log('loaded drivers: ', that.drivers)
+          },
+        })
       },
       cmpNames: function (x, y) {
         var name1 = x.name ? x.name.toLowerCase() : ''
         var name2 = y.name ? y.name.toLowerCase() : ''
-        return name1 < name2 ? -1 : name1 === name2 ? 0 : +1
+        return name1 < name2 ? -1 : name1 > name2 ? +1 : 0
       },
     },
   }
