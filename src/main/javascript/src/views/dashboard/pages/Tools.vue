b218def22851890f8dc6cf679b6269956ad3e2c1
luca-vercelli <luca.vercelli.to@gmail.com>
2020-12-22 23:49:12 +0100

    tools page

---
+++ src/main/javascript/src/views/dashboard/pages/Tools.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container
+    id="regular-tables"
+    fluid
+    tag="section"
+  >
+    <base-material-card
+      icon="mdi-tools"
+      title="Tools"
+      class="px-5 py-3"
+    >
+      <v-btn
+        class="mr-4"
+        target="_blank"
+        @click="checkDriverClasses"
+      >
+        <i
+          aria-hidden="true"
+          class="v-icon notranslate mdi mdi-android theme--dark"
+        />
+        Scan for driver classes
+      </v-btn>
+    </base-material-card>
+  </v-container>
+</template>
+
+<script>
+  var $ = require('jquery')
+  export default {
+    name: 'Tools',
+
+    data () {
+      return {
+        enableMock: process.env.VUE_APP_MOCK === 'true',
+      }
+    },
+
+    methods: {
+      checkDriverClasses: function () {
+        this.editEnabled = false
+        var url = this.enableMock ? process.env.BASE_URL + 'mock/JustGetOk' : process.env.BASE_URL + 'ws/CheckAllDrivers'
+        var that = this
+        $.ajax({
+          type: this.enableMock ? 'GET' : 'POST',
+          url: url,
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('authToken'),
+          },
+          success: function (data, status) {
+            that.$emit('notify', { message: 'Done.', type: 'success' })
+          },
+          error: function (response) {
+            that.$emit('ajax-error', response)
+          },
+        })
+      },
+    },
+  }
+</script>
