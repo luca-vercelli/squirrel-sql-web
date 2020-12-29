c5583736be03856eb4696bc649c5cc19b4eb4b86
Luca Vercelli <luca.vercelli.to@gmail.com>
2020-12-29 08:59:06 +0100

    clean

--- src/main/java/net/sourceforge/squirrel_sql/ws/resources/SqlTabEndpoint.java
+++ src/main/java/net/sourceforge/squirrel_sql/ws/resources/SqlTabEndpoint.java
@@ -8,7 +8,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response.Status;
